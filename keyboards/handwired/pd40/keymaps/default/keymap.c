29bc16d435242d0df5a680622f8843f22c53f70f
Peter Dye <pjad98@gmail.com>
2021-11-15 20:16:23 -0500

    moving the enter key to where the left space key was

--- keyboards/handwired/pd40/keymaps/default/keymap.c
+++ keyboards/handwired/pd40/keymaps/default/keymap.c
@@ -33,10 +33,10 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [BASE] = LAYOUT(
-        KC_ESC,    KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
-        KC_TAB,    KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
-       KC_LSFT,    KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
-       KC_LCTL, KC_LALT,MO(MOUSE), KC_LGUI,MO(LEFT), KC_SPC,  KC_SPC,MO(RIGHT), KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT
+        KC_ESC,    KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
+        KC_TAB,    KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,     KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+       KC_LSFT,    KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,     KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
+       KC_LCTL, KC_LALT,MO(MOUSE), KC_LGUI,MO(LEFT),  KC_ENT,  KC_SPC,MO(RIGHT), KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT
     ),
 
     [LEFT] = LAYOUT(
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          RESET, XXXXXXX, KC_ACL0, KC_ACL1, KC_ACL2, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
        XXXXXXX, XXXXXXX, KC_BTN1, KC_BTN2, KC_BTN3, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
        _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-       _______, _______, _______, XXXXXXX, _______, XXXXXXX, XXXXXXX, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R
+       _______, _______, _______, XXXXXXX, _______, _______, XXXXXXX, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R
     )
 };
 
