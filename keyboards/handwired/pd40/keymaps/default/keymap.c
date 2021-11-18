0f3a96fd13dcc5fa77a2c06900d929d29e5762be
Peter Dye <pjad98@gmail.com>
2021-11-17 19:38:54 -0500

    small keymap update

--- keyboards/handwired/pd40/keymaps/default/keymap.c
+++ keyboards/handwired/pd40/keymaps/default/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [LEFT] = LAYOUT(
         KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
-       _______, XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX,  KC_EQL, KC_MINS, KC_LBRC, KC_RBRC, KC_BSLS,  KC_ENT,
+       _______, XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX,  KC_EQL, KC_MINS, KC_LBRC, KC_RBRC, KC_BSLS, _______,
        _______, XXXXXXX, KC_MPRV, KC_MSTP, KC_MNXT, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, _______,
        _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
     ),
